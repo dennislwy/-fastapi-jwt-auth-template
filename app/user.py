3654fa40f412352a5c5d6b87affd2867196a8222
Dennis Lee <dennis@gsfcorp.com>
2024-03-06 12:23:29 +0800

    Update GitHub Actions workflows and user.py***

--- app/user.py
+++ app/user.py
@@ -47,34 +47,67 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)],
                            active: bool = False,
                            verified: bool = False,
                            superuser: bool = False) -> User:
+    """
+    Retrieve the current user based on the provided JWT token.
 
+    This function decodes the JWT token to get the user_id and then retrieves the user from the
+    database. It also checks if the user is active, verified, and a superuser based on the provided
+    parameters.
+
+    Args:
+        token (str): The JWT token of the user.
+        db (AsyncSession): The database async session.
+        active (bool, optional): Whether the user needs to be active. Defaults to False.
+        verified (bool, optional): Whether the user needs to be verified. Defaults to False.
+        superuser (bool, optional): Whether the user needs to be a superuser. Defaults to False.
+
+    Returns:
+        User: The retrieved user object.
+
+    Raises:
+        HTTPException: If the token is invalid, the user is not found, the user is inactive, the
+        user is unverified, or the user is not a superuser.
+    """
     status_code = status.HTTP_401_UNAUTHORIZED
 
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        # Set the options for the JWT token validation
+        options = {"require_exp": True, "require_sub": True, "require_jti": True}
+
+        # Decode, validate JWT token and get the payload
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM], options=options)
+
+        # Get the user_id from the payload
         user_id: str = payload.get("sub")
 
+        # If the user_id is None, the token is invalid
         if user_id is None:
             raise HTTPException(status_code=status_code, detail="Invalid token")
 
     except (JWTError, ValidationError) as exc:
+        # If there is an error while decoding the token, it is invalid
         raise HTTPException(status_code=status_code, detail=str(exc)) from exc
 
+    # Retrieve the user from the database
     user = await get_user_by_id(user_id=user_id, db=db)
 
     if user:
         status_code = status.HTTP_403_FORBIDDEN
+        # If the user needs to be active and is not, raise an exception
         if active and not user.is_active:
             status_code = status.HTTP_401_UNAUTHORIZED
             raise HTTPException(status_code=status_code, detail="Inactive user")
 
+        # If the user needs to be verified and is not, raise an exception
         if verified and not user.is_verified:
             raise HTTPException(status_code=status_code, detail="Unverified user")
 
+        # If the user needs to be a superuser and is not, raise an exception
         if superuser and not user.is_superuser:
             raise HTTPException(status_code=status_code, detail="Superuser required")
 
     else:
+        # If the user is not found in the database, raise an exception
         raise HTTPException(status_code=status_code, detail="User not found")
 
     return user
@@ -84,8 +117,8 @@ async def get_user_by_id(user_id: str, db: Annotated[AsyncSession, Depends(get_d
     Retrieve a user from the database based on the provided user_id.
 
     Args:
+        user_id (str): The ID of the user to retrieve.
         db (AsyncSession): The database async session.
-        user_id (str, optional): The ID of the user to retrieve. Defaults to None.
 
     Returns:
         User: The retrieved user object or None if no user is found.
@@ -97,8 +130,8 @@ async def get_user_by_email(email: str, db: Annotated[AsyncSession, Depends(get_
     Retrieve a user from the database based on the provided email.
 
     Args:
+        email (str, optional): The email of the user to retrieve
         db (AsyncSession): The database async session.
-        email (str, optional): The email of the user to retrieve. Defaults to None.
 
     Returns:
         User: The retrieved user object or None if no user is found.
